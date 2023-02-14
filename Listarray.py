f770eab0aa6c67ee03a1406256766ac2a62feb27
kasraHeidarinezhad <Kasra.Heidarinezhad@gmail.com>
2023-02-14 13:44:07 -0800

    Final

--- Listarray.py
+++
@@ -1,7 +0,0 @@
-urlList = []
-urlList.append("'https://www.zillow.com/homes/for_sale/Vancouver,-BC_rb/'")
-
-for item in range(2,50):
-    urlList.append("'https://www.zillow.com/homes/for_sale/Vancouver,-BC_rb/" + str(item)+"_p/'")
-
-print(urlList,"\n")
